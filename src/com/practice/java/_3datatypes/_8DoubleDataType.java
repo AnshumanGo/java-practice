898c5109054c90800c9165fc245413d194956590
AnshumanGo <anshuman.g.cs@gmail.com>
2024-06-17 09:58:55 +0530

    _3datatypes_8DoubleDataType

---
+++ src/com/practice/java/_3datatypes/_8DoubleDataType.java
@@ -0,0 +1,16 @@
+package com.practice.java._3datatypes;
+
+public class _8DoubleDataType {
+
+    double doubleOne = 234.59;
+    double doubleTwo;
+
+    public static void main(String[] args) {
+
+        System.out.println("Value of double data longOne : " + new _8DoubleDataType().doubleOne);
+        System.out.println("Default value of double data longOne : " + new _8DoubleDataType().doubleTwo);
+
+        System.out.println("Max value of double data type : " + Double.MAX_VALUE);
+        System.out.println("Min value of double data type : " + Double.MIN_VALUE);
+    }
+}
