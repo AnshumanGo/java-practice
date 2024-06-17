06f68876c39443f7463a5af943b7ed3a4a78acba
AnshumanGo <anshuman.g.cs@gmail.com>
2024-06-17 11:14:14 +0530

    _4operators_8BitwiseOrOperator

---
+++ src/com/practice/java/_4operators/_8BitwiseOrOperator.java
@@ -0,0 +1,20 @@
+package com.practice.java._4operators;
+
+import static com.practice.java._4operators._5LogicalAndOperator.isGreaterThanIntValue;
+import static com.practice.java._4operators._5LogicalAndOperator.isLessThanIntValue;
+
+public class _8BitwiseOrOperator {
+
+    public static void main(String[] args) {
+
+        //Bitwise OR operator check both boolean condition even if first condition is false or true
+        int a = 10, b = 20, c = 5;
+
+        System.out.println(isLessThanIntValue(a, b) | isGreaterThanIntValue(a, c));
+        System.out.println(isLessThanIntValue(a, b) | isLessThanIntValue(a, c));
+        System.out.println(isGreaterThanIntValue(a, b) | isLessThanIntValue(a, c));
+        System.out.println(isGreaterThanIntValue(a, b) | isGreaterThanIntValue(a, c));
+
+        System.out.println(a | b);
+    }
+}
