3bf4ca0c2219e4bb90c34e928e408ff1bdd164c2
AnshumanGo <anshuman.g.cs@gmail.com>
2024-06-18 13:03:40 +0530

    _5controlstatements_1ifelsestatement_2IfElseStatement

---
+++ src/com/practice/java/_5controlstatements/_1ifelsestatement/_2IfElseStatement.java
@@ -0,0 +1,13 @@
+package com.practice.java._5controlstatements._1ifelsestatement;
+
+public class _2IfElseStatement {
+
+    public static void main(String[] args) {
+
+        int intNumber = 17;
+        if (intNumber % 2 == 0)
+            System.out.println("Present number is even number");
+        else
+            System.out.println("Present number is odd number");
+    }
+}
