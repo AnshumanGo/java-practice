4ef3c589bedc6371c3be8f0168664c18ed9e2452
AnshumanGo <anshuman.g.cs@gmail.com>
2024-06-26 09:21:30 +0530

    _23lambdaexpressionsandstreams_4EatStreamCollect formatting

--- src/com/practice/java/_23lambdaexpressionsandstreams/_4EatStreamCollect.java
+++ src/com/practice/java/_23lambdaexpressionsandstreams/_4EatStreamCollect.java
@@ -13,6 +13,7 @@ public class _4EatStreamCollect {
 	private String foodItemSupplier;
 	private float foodItemQuantity;
 	private float foodItemUnitPrice;
+
 	public String getFoodItemName() {
 		return foodItemName;
 	}
@@ -68,42 +69,43 @@ public class _4EatStreamCollect {
 	public void setFoodItemUnitPrice(float foodItemUnitPrice) {
 		this.foodItemUnitPrice = foodItemUnitPrice;
 	}
-	public _4EatStreamCollect(String foodItemName, String foodItemType, String foodItemSubType, String foodItemManufacturer, String foodItemSupplier, float foodItemQuantity, float foodItemUnitPrice) {  
-	    this.foodItemName = foodItemName;
-	    this.foodItemType = foodItemType;
-	    this.foodItemSubType = foodItemSubType;
-	    this.foodItemManufacturer = foodItemManufacturer;
-	    this.foodItemSupplier = foodItemSupplier;
-	    this.foodItemQuantity = foodItemQuantity;
-	    this.foodItemUnitPrice = foodItemUnitPrice;
+
+	public _4EatStreamCollect(String foodItemName, String foodItemType, String foodItemSubType,
+			String foodItemManufacturer, String foodItemSupplier, float foodItemQuantity, float foodItemUnitPrice) {
+		this.foodItemName = foodItemName;
+		this.foodItemType = foodItemType;
+		this.foodItemSubType = foodItemSubType;
+		this.foodItemManufacturer = foodItemManufacturer;
+		this.foodItemSupplier = foodItemSupplier;
+		this.foodItemQuantity = foodItemQuantity;
+		this.foodItemUnitPrice = foodItemUnitPrice;
 	}
 
 	@Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result
-                + ((foodItemName == null) ? 0 : foodItemName.hashCode());
-        return result;
-    }
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((foodItemName == null) ? 0 : foodItemName.hashCode());
+		return result;
+	}
 
-    @Override
-    public boolean equals(final Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        final _4EatStreamCollect other = (_4EatStreamCollect) obj;
-        if (foodItemName == null) {
-            if (other.foodItemName != null)
-                return false;
-        } else if (!foodItemName.equals(other.foodItemName))
-            return false;
-        return true;
-    }
-	
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final _4EatStreamCollect other = (_4EatStreamCollect) obj;
+		if (foodItemName == null) {
+			if (other.foodItemName != null)
+				return false;
+		} else if (!foodItemName.equals(other.foodItemName))
+			return false;
+		return true;
+	}
+
 	public static void main(String[] args) {
 		List<_4EatStreamCollect> foodOrder = new ArrayList<_4EatStreamCollect>();
 		foodOrder.add(new _4EatStreamCollect("Paneer Hariyali Tikka", "Veg", "Paneer Base", "Local", "Local", 1f, 63f));
@@ -112,16 +114,18 @@ public class _4EatStreamCollect {
 		foodOrder.add(new _4EatStreamCollect("Malai Kofta", "Veg", "Kofta milk based base", "Local", "Local", 1f, 63f));
 		foodOrder.add(new _4EatStreamCollect("Daal Tadka", "Veg", "Daal base", "Local", "Local", 1f, 45f));
 		foodOrder.add(new _4EatStreamCollect("Ghee phulka", "Veg", "Phulka roti", "Local", "Local", 1f, 10f));
-		foodOrder.add(new _4EatStreamCollect("Veg Paneer Dum Biryani", "Veg", "Phulka roti", "Local", "Local", 1f, 110f));
-		
-		System.out.println("Number of different food items in food order : "+foodOrder.stream().distinct().count());
-		
-		_4EatStreamCollect firstVegFoodItemInList = foodOrder.stream().filter(item -> item.getFoodItemType().equalsIgnoreCase("veg")).findFirst().orElse(null);
-		
-		if(firstVegFoodItemInList) {
-			System.out.println("First veg  item in order : "+firstVegFoodItemInList);
+		foodOrder.add(
+				new _4EatStreamCollect("Veg Paneer Dum Biryani", "Veg", "Phulka roti", "Local", "Local", 1f, 110f));
+
+		System.out.println("Number of different food items in food order : " + foodOrder.stream().distinct().count());
+
+		_4EatStreamCollect firstVegFoodItemInList = foodOrder.stream()
+				.filter(item -> item.getFoodItemType().equalsIgnoreCase("veg")).findFirst().orElse(null);
+
+		if (firstVegFoodItemInList != null) {
+			System.out.println("First veg  item in order : " + firstVegFoodItemInList);
 		} else {
 			System.out.println("No veg ordered");
 		}
-		}
+	}
 }
