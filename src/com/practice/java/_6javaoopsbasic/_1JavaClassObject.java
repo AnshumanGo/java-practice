da233e1b356d219af4f9bdfe86bc483dbd94f685
AnshumanGo <anshuman.g.cs@gmail.com>
2025-07-23 22:44:23 +0530

    23rdJuly2025-1

--- src/com/practice/java/_6javaoopsbasic/_1JavaClassObject.java
+++ src/com/practice/java/_6javaoopsbasic/_1JavaClassObject.java
@@ -14,16 +14,13 @@ public class _1JavaClassObject {
     public static void main(String[] args) {
 
         _1JavaClassObject javaClassObjectOne = new _1JavaClassObject();
-        //Object and Class Example: Initialization through reference
         javaClassObjectOne.id = 1;
         System.out.println("Java class object one id : " + javaClassObjectOne.id);
 
-        //Object and Class Example: Initialization through a constructor
         _1JavaClassObject javaClassObjectTwo = new _1JavaClassObject(2);
         System.out.println("Java class object two id : " + javaClassObjectTwo.id);
 
         _1JavaClassObject javaClassObjectThree = new _1JavaClassObject();
-        //Object and Class Example: Initialization through method
         javaClassObjectThree.setId(3);
         System.out.println("Java class object three id : " + javaClassObjectThree.getId());
 
